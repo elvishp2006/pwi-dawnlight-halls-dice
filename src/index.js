e1c6b271603e2712c071253c5b292fa641150ee8
Elvis Henrique Pereira <elvishp2006@gmail.com>
2017-10-23 08:59:17 -0200

    Document.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 'use strict';
 
 class Dice {
+    /**
+     * Passe os turnos para serem realizados na construção do dado.
+     * Opcionalmente você pode passar as faces atuais.
+     * @param {string} turns
+     * @param {object} faces
+     */
     constructor(turns, faces = { top: 2, front: 1, right: 4 }) {
         this.init( faces );
         this.doTurns( turns );
     }
 
+    /**
+     * Inicializa o dado com as faces recebidas via parâmetro.
+     * @param {string} faces
+     */
     init(faces) {
         this.top = faces.top;
         this.front = faces.front;
         this.right = faces.right;
     }
 
+    /**
+     * Realiza os turnos para obtenção de novas faces.
+     * Ex.: URR
+     * @param {string} turns
+     */
     doTurns(turns) {
         if ( ! turns ) {
             return;
@@ -32,6 +47,9 @@ class Dice {
         });
     }
 
+    /**
+     * Rola o dado para cima.
+     */
     turnUp() {
         let bottom = this.getBottom();
 
@@ -39,10 +57,16 @@ class Dice {
         this.front = bottom;
     }
 
+    /**
+     * Obtem o valor da face abaixo.
+     */
     getBottom() {
         return Math.abs( this.top - 7 );
     }
 
+    /**
+     * Rola o dado para a direita.
+     */
     turnRight() {
         let left = this.getLeft();
 
@@ -50,14 +74,23 @@ class Dice {
         this.front = left;
     }
 
+    /**
+     * Obtem o valor da face à esquerda.
+     */
     getLeft() {
         return Math.abs( this.right - 7 );
     }
 
+    /**
+     * Obtem o valor altual.
+     */
     get current() {
         return this.front;
     }
 
+    /**
+     * Retorna o valor de todas as faces.
+     */
     get faces() {
         return {
             top: this.top,
